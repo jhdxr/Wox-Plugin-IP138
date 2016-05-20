0cd6ae7da5c4340a6e3efd0bc14c74e34cd88e71
jhdxr <me@jhdxr.com>
2016-05-20 17:27:08 +0800

    first version

---
+++ Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Wox.Plugin.IP138
+{
+
+    public class Main : IPlugin
+    {
+
+        private string ip138Url = @"http://ip138.com/ips138.asp?ip=";
+
+        public void Init(PluginInitContext context)
+        {
+            
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+
+            string ip = query.Search;
+
+            var client = new WebClient();
+            string html = Encoding.GetEncoding("GBK").GetString(client.DownloadData(ip138Url + ip));
+
+            Match li = Regex.Match(html, @"<ul class=""ul1"">(.*?)</ul>");
+            MatchCollection matches = Regex.Matches(li.Groups[1].Value, @"<li>(.+?)：(.+?)</li>");
+            foreach (Match match in matches)
+            {
+                results.Add(new Result()
+                {
+                    Title = match.Groups[2].Value,
+                    SubTitle = match.Groups[1].Value,
+                    IcoPath = "Images\\ip138.ico"
+                });
+            }
+
+            return results;
+        }
+
+    }
+}
